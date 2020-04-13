25700340e8a37c00762639621385d7587d1ed412
Paul Tonning <paul.tonning@student.vvc.edu>
2020-04-13 21:01:31 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lists
 Lists are sequences, a sequence is a data structure
+[Textbook Reading]
 [Data Structures in Python](https://docs.python.org/3/tutorial/datastructures.html)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-# lists are sequences
-# To create an empty list, use this assignment
-my_list = []
-# because string is an iterable, it will become a
-# list of characters, looks at the following
-my_list += 'Hello';
-print(my_list)
-# it should print ['H', 'e', 'l', 'l', 'o']
-# lists can also have other lists as one of the items
-my_list += [['one','two','three'],'orange','red']
-print(my_list)
+def lists_as_sequences():
+    # lists are sequences
+    # To create an empty list, use this assignment
+    my_list = []
+    # because string is an iterable, it will become a
+    # list of characters, looks at the following
+    my_list += 'Hello';
+    print(my_list)
+    # it should print ['H', 'e', 'l', 'l', 'o']
+    # lists can also have other lists as one of the items
+    my_list += [['one','two','three'],'orange','red']
+    print(my_list)
+    
+    # lists are mutable, this means that the list elements can be changed.
+    my_list[0] = 'Changed Item'
+    print(my_list)
